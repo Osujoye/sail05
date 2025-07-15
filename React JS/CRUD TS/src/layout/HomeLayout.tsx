53823b0fb673446bee74c7c37bf72ba615fb1015
Sammy <olorundasamuel2@gmail.com>
2025-07-15 07:40:26 +0100

    Agt request from an API with axios

--- React JS/CRUD TS/src/layout/HomeLayout.tsx
+++ React JS/CRUD TS/src/layout/HomeLayout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../static/Header";
-import Footer from "../static/Footer";
+// import Footer from "../static/Footer";
 
 const HomeLayout = () => {
   return (
     <div>
       <Header />
       <Outlet />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
--- React JS/CRUD TS/src/static/Header.tsx
+++ React JS/CRUD TS/src/static/Header.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
+    <div className=" h-20 bg-purple-400 flex justify-center items-center gap-10 font-bold">
       <NavLink to="/">
         <nav>Home</nav>
       </NavLink>
